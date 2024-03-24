c2b6ca182b5ba78d23d21cb5c67e4507ec87eecc
Mathieu Gamache <mathg99@hotmail.com>
2024-03-24 12:59:27 -0400

    Add some fun with OpenAPI spec

---
+++ AspNetPlayground/AspNetPlayground/Api/Controllers/OpenAPIController.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+/// <summary>
+/// [ApiController] is doing a lots of nice things for us:
+/// - Automatically validate the request
+/// - Automatically return a 400 Bad Request if the request is invalid
+/// - Automatically return a 500 Internal Server Error if an exception is thrown (ProblemDetails in Development, Empty body in Production)
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class OpenApiController: ControllerBase
+{
+    [HttpGet]
+    [Route("get-example")]
+    [ProducesResponseType<ExampleResponse>((int) HttpStatusCode.OK)]
+    [ProducesResponseType<ProblemDetails>((int) HttpStatusCode.BadRequest)]
+    [ProducesResponseType((int) HttpStatusCode.TooManyRequests)]
+    [ProducesResponseType((int) HttpStatusCode.InternalServerError)]
+    public IActionResult Get([FromQuery] ExampleRequest request)
+    {
+        switch (request.Type)
+        {
+            case ResponseType.Ok:
+                return this.Ok(new ExampleResponse { Message = request.Count.ToString() });
+            case ResponseType.BadRequest:
+                return Problem("This is a bad request.", statusCode: (int) HttpStatusCode.BadRequest);
+            case ResponseType.TooManyRequests:
+                // Missing how to add Retry-After header description in OpenAPI
+                this.Response.Headers.Append("Retry-After", "120");
+                return StatusCode((int)HttpStatusCode.TooManyRequests);
+            case ResponseType.Exception:
+                throw new Exception("This is an example error.");
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+    
+    [HttpPost]
+    [Route("post-example")]
+    public IActionResult Post(ExampleRequest request)
+    {
+        switch (request.Type)
+        {
+            case ResponseType.Ok:
+                return this.Ok(new ExampleResponse { Message = request.Count.ToString() });
+            case ResponseType.BadRequest:
+                return Problem("This is a bad request.", statusCode: (int) HttpStatusCode.BadRequest);
+            case ResponseType.TooManyRequests:
+                this.Response.Headers.Append("Retry-After", "120");
+                return StatusCode((int)HttpStatusCode.TooManyRequests);
+            case ResponseType.Exception:
+                throw new Exception("This is an example error.");
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+        
+    public class ExampleRequest
+    {
+        [Required]
+        public ResponseType Type { get; set; }
+        
+        [Required]
+        [Range(0, 10)]
+        public int Count { get; set; }
+    }
+    
+    public enum ResponseType
+    {
+        Ok,
+        BadRequest,
+        TooManyRequests,
+        Exception,
+    }
+    
+    public class ExampleResponse
+    {
+        [Required] 
+        public string Message { get; set; } = string.Empty;
+    }
+}
--- AspNetPlayground/AspNetPlayground/Api/Startup.cs
+++ AspNetPlayground/AspNetPlayground/Api/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Api.HttpClients;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Api.HttpClients;
 using Api.OpenApi;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api;
 
@@ -7,8 +10,18 @@ public static class Startup
 {
     public static IHostApplicationBuilder RegisterDependencies(this IHostApplicationBuilder builder)
     {
-        // Enable to discovers controllers (vs minimal API)
-        builder.Services.AddControllers();
+        builder.Services.AddControllers(options =>
+        {
+            options.Filters.Add(new ProducesAttribute("application/json"));
+            options.Filters.Add(new ConsumesAttribute("application/json"));
+
+        });
+        
+        builder.Services.Configure<JsonOptions>(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+            
+        });
 
         // ** Error Handling **
         // Can be extended to include other parameters by default
@@ -25,30 +38,6 @@ public static class Startup
     // TODO: Is it only middleware or something pipelines?
     public static WebApplication ConfigureMiddleware(this WebApplication app)
     {
-        // Route Matching Middleware: find best endpoint based on the request
-        // When using WebApplicationBuilder it is automatically added
-        // app.UseRouting();
-        
-        // Add endpoints execution to the middleware pipeline
-        // When using WebApplicationBuilder it is automatically added
-        // app.UseEndpoints();
-
-        // ** Error Handling **
-        
-        // Translate exception to Problem Details (exclude some sensitive information)
-        // TODO: More to it!
-        app.UseExceptionHandler();
-
-        // Convert any response with a status code between 400 and 599 without a body to a ProblemDetails response
-        app.UseStatusCodePages();
-        
-        // TODO
-        if (app.Environment.IsDevelopment())
-        {
-            // Will add the exception details to the response (callstack, headers, etc)
-            app.UseDeveloperExceptionPage();
-        }
-        
         if (!app.Environment.IsDevelopment())
         {
             // Enforce HTTPS
@@ -58,7 +47,42 @@ public static class Startup
         // TODO: Health check
         
         app.UseOpenApi();
+
+        // Define endpoints for convention based controllers
+        app.MapControllers();
         
+        
+        return app;
+    }
+    
+    public static WebApplication ConfigureExperimentalMiddleware(this WebApplication app)
+    {
+        // Route Matching Middleware: find best endpoint based on the request
+        // When using WebApplicationBuilder it is automatically added
+        app.UseRouting();
+        
+        // Add endpoints execution to the middleware pipeline
+        // When using WebApplicationBuilder it is automatically added
+        // Old school way of doing this, should now do app.MapControllers()
+#pragma warning disable ASP0014
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+#pragma warning restore ASP0014
+
+        // ** Error Handling **
+        
+        // Would redirect use to /error, not wanted for API
+        app.UseExceptionHandler("/error");
+
+        // Add a simple body to the response when there is an error and no body
+        app.UseStatusCodePages();
+        
+        // Return a HTML page with exception details: callstack, headers, etc,...
+        // Weird for an API
+        //app.UseDeveloperExceptionPage();
+
         // Minimal API Example
         // Define an endpoint: something that can be selected by matching URL and executed
         app
@@ -67,10 +91,6 @@ public static class Startup
             .WithSummary("Hello world from a minimal API!")
             //.RequireAuthorization() // You can add metadata to endpoint which can be used by middleware
             .WithOpenApi();
-        
-        // Define endpoints for convention based controllers
-        app.MapControllers();
-        
         
         return app;
     }
